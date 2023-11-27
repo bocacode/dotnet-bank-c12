91eeeec5e76e177cc12a33c3c9555d038551e2cd
Damian Montero <photoca@hotmail.com>
2023-11-27 10:40:52 -0500

    in between

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+int age = 50;
+// age = "fifty"; // can't do this
+
+string name = "Damian";
+ 
+Console.WriteLine($"your name is  {name} and you're {age} years old");
+
+
